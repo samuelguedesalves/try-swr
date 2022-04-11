7679ebb349e831094727eb0dd089b84db9055bdb
samuel guedes <guedes.works7@gmail.com>
2022-04-10 23:13:18 -0400

    feat: create header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+export const Header = () => {
+  return (
+    <Container>
+      <div className="content">
+        <h1>Users list</h1>
+      </div>
+    </Container>
+  )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  background: #eee;
+
+  .content {
+    max-width: 1080px;
+    margin: 0 auto;
+    padding: 0 12px;
+  }
+`;
